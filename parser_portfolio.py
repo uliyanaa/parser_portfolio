b3439ebd58e03d75e14999e75f99d5bc6409f28c
uliyanaa <yuliana.storchak@gmail.com>
2023-05-09 17:48:50 +0300

    step 2

--- parser_portfolio.py
+++ parser_portfolio.py
@@ -1,3 +1,96 @@
 from zipfile import ZipFile
 import json
 import time
+
+import requests
+import csv
+import datetime
+from bs4 import BeautifulSoup
+
+def get_data():
+    cur_date = datetime.datetime.now().strftime("%d_%m_%Y_%H_%M")
+    with open(f'labirint_{cur_date}.csv', 'w') as file:
+        writer = csv.writer(file)
+        
+        writer.wroterow(
+            (
+                'Название книги',
+                'Автор',
+                'Издательство',
+                'Цена со скидкой',
+                'Цена без скидки',
+                'Процент скидки',
+                'Наличие на складе'
+            )
+        )
+        
+    headers = {
+        "accept": "text / html, * / *;q = 0.01",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "ru, en;q = 0.9, en - GB;q = 0.8, en - US;q = 0.7",
+        'user-agent': 'Mozilla/5.0(Windows NT 10.0;Win64;x64) AppleWebKit/537.36(KHTML, likeGecko'') Chrome/100.0.4896.60Safari/537.36Edg/100.0.1185.29'
+    }
+    
+    url = 'https://www.labirint.ru/genres/2308/?availible=1&paperbook=1&display=table'
+    
+    r = requests.get(url=url, headers=headers)
+    
+    soup = BeautifulSoup(r.text, 'lxml')
+    pages_count = int(soup.find('div', class_="pagination-numbers").find_all("a")[-1].text)
+    books_data = []
+    for page in range(1, pages_count + 1):
+        url = f'https://www.labirint.ru/genres/2308/?availible=1&paperbook=1&display=table&page={page}'
+        
+        r = requests.get(url=url, headers=headers)
+        soup = BeautifulSoup(r.text, 'lxml')
+        books_items = soup.find("tbody", class_="products-table__body").find_all("tr")
+        
+        for bi in books_items:
+            book_data = bi.find_all("td")
+            print(books_data) 
+            try:
+                book_title = book_data[0].find('a').text.strip()
+            except:
+                book_title = "unnamed"
+                
+            try:
+                book_author = book_data[1].find('a').text.strip()
+            except:
+                book_author = "no author"
+                
+            try:
+                book_publishing = book_data[2].find('a').text.strip()
+            except:
+                book_publishing = "no izdatelstvo"
+                
+            try:
+                book_new_price = int(book_data[3].find('div', class_='price').find('span', class_='price-val').find('span').text.strip().replace(" ", ""))
+            except:
+                book_new_price = 'Нет новой цены'
+            
+            try:
+                book_old_price = int(book_data[3].find('span', class_='price-gray').text.strip().replace(" ", ""))
+            except:
+                book_old_price = 'Нет старой цены'
+                
+            try:
+                book_sale = round(((book_old_price - book_new_price) / book_old_price) * 100)
+            except:
+                book_sale = "no sale"
+            
+            try:
+                book_status = book_data[-1].text.strip()
+            except:
+                book_status = 'no status'
+            books_data.append(
+                {
+                    'book_title': book_title,
+                    'book_author': book_author,
+                    'book_publishing': book_publishing,
+                    'book_new_price': book_new_price,
+                    'book_old_price': book_old_price,
+                    'book_sale': book_sale,
+                    'book_status': book_status,
+                }
+            )
+        
